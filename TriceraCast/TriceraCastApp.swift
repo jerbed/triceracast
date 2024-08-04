56cfe595af4b49c4da5b15689fbfee917e61fc4b
Yiqing Li <potalit@gmail.com>
2024-08-04 11:31:55 -0400

    first commit

---
+++ TriceraCast/TriceraCastApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriceraCastApp.swift
+//  TriceraCast
+//
+//  Created by YiQing Li on 8/4/24.
+//
+
+import SwiftUI
+
+@main
+struct TriceraCastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
